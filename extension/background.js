550b30e43fa1251f00afc7b174ba7bb4413af89a
Christian Aguilar <cbaguilar@ucla.edu>
2021-03-27 22:38:07 -0700

    added successful action to context menu

--- extension/background.js
+++ extension/background.js
@@ -4,3 +4,20 @@ chrome.runtime.onInstalled.addListener(() => {
 	chrome.storage.sync.set({color});
 	console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+searchUrbanDict = function (word) {
+	var query = word.selectionText;
+	chrome.tabs.create({url: "http://www.urbandictionary.com/define.php?term=" + query});
+}
+
+chrome.runtime.onInstalled.addListener(function() {
+	console.log("Adding eddit..");
+	chrome.contextMenus.create({
+	id: 'search-dict',
+	title: "Search in urbdict",
+	contexts: ["selection"],
+	},(res)=>{console.log(res)});
+});
+chrome.contextMenus.onClicked.addListener(function() {
+	console.log("FIRED");
+});
